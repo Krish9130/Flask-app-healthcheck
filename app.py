42f5c71fe5c6ef03a33ec45f5a9fb3bb01c9eeb7
Krushna Sonawane <105139116+Krish9130@users.noreply.github.com>
2025-01-24 15:49:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 import logging
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
@@ -68,4 +68,3 @@ def handle_exception(e):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
-'''
